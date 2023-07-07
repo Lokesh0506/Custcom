0c4a572b51608a2ccca54c2c48fa71f3b55a81b8
Custcom <custcom.proj@gmail.com>
2023-07-07 17:57:44 +0530

    cart +- button added

--- client/src/components/Cartbody.js
+++ client/src/components/Cartbody.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { H1, H2, H3, IMG, P, A } from './tags';
-import { setStyle, setContent, setHref, setSrc } from './dbfunctions';
 import Axios from 'axios';
 import './cartbody.css';
+import { BUTTON } from './tags';
 
 const Cartbody = (props) => {
   const [data, setData] = useState([]);
@@ -27,76 +26,68 @@ const Cartbody = (props) => {
       return 'no img';
     }
   };
+
   function sendUpdateRequest(pid, quantity) {
     Axios
       .post("/cart/add", { pid, quantity })
       .then((response) => {
-       
         const updatedQuantity = response.data.quantity;
         console.log("Updated quantity:", updatedQuantity);
-       
       })
       .catch((error) => {
-        
         console.log("An error occurred:", error.message);
       });
   }
   
   function incrementQuantity(pid) {
-    const quantity = parseInt(document.getElementById("texqan").value);
-    quantity++;
-  
-
-    sendUpdateRequest(pid, quantity);
+    const quantityElement = document.getElementById(`texqan-${pid}`);
+    if (quantityElement) {
+      let quantity = parseInt(quantityElement.value);
+      alert("hii");
+      quantity++;
+      sendUpdateRequest(pid, quantity);
+    }
   }
   
   function decrementQuantity(pid) {
-    let quantity = parseInt(document.getElementById("texqan").value);
-    quantity--;
-  
-    sendUpdateRequest(pid, quantity);
+    const quantityElement = document.getElementById(`texqan-${pid}`);
+    if (quantityElement) {
+      let quantity = parseInt(quantityElement.value);
+      quantity--;
+      sendUpdateRequest(pid, quantity);
+    }
   }
 
-    function handleInputChange(event, pid) {
-      const quantity = parseInt(event.target.value);
-sendUpdateRequest(pid, quantity);
-    }
   return (
     <div className='Cartpage'>
-     
       {data.cart && data.cart.length > 0 && data.inventory && data.inventory.length > 0 && (
         data.cart.map((cartItem, index) => {
-          const { pid } = cartItem;
+          const { pid , quantity} = cartItem;
           const inventoryArr = data.inventory;
-  
+
           const inventoryItem = inventoryArr.find(item => item[0].pid === pid);
-  
+
           if (inventoryItem) {
             const { pname, price, authorname } = inventoryItem[0];
-  
+
             return (
               <div key={pid} className='cartprd'>
-         
-                <IMG
-                  src={setSrcprod(inventoryItem, pid)}
-                  key={`${pid}-${index}`}
-                />
-              
+                <img src={setSrcprod(inventoryItem, pid)} alt={`${pid}-${index}`} />
                 <h2 id="name">{pname}</h2>
                 <h2 id="author">{authorname}</h2>
                 <h2 id="price">₹{price}</h2>
-                
+                <BUTTON id="sub" onClick={()=>decrementQuantity(pid)} value="-" />
+                <input type='number' id={`texqan-${pid}`} defaultValue={quantity} />
+                <BUTTON id="sub" onClick={()=>incrementQuantity(pid)} value="+" />
               </div>
             );
           } else {
-            return <h1>Some error occured</h1>;
+            return <h1>Some error occurred</h1>;
           }
         })
       )}
     </div>
   );
-  
-  
 }
 
 export default Cartbody;
