1d1520ae1acb8ea583eaaca5d113d03071919173
Custcom <custcom.proj@gmail.com>
2023-07-23 17:57:07 +0530

    Custcom v1

--- client/src/login.js
+++ client/src/login.js
@@ -3,89 +3,97 @@ import "./login.css"
 import logo from './components/images/logo.png'
 import { useNavigate } from 'react-router-dom';
 import Axios from 'axios';
+import styles from './login.module.css'
 
 
 const Login = () => {
-    
-        const [username, setUsername] = useState('');
-        const [mobilenumber, setMobileno] = useState('');
-        const [password, setPassword] = useState('');
-        const navigate = useNavigate();
-        var enableHover='false';
-      
-        const handleSubmit = async (e) => {
-          e.preventDefault();
-      
-          try {
-            const response = await Axios.post('http://localhost:8080/login', {
-              username,
-              password,
-              mobilenumber
-            });
-            
 
-            if (response.data==='invalidUsername') {
-              alert("Invalid Username");
-          }
-      
-            else if (response.data==="toAdmin") {
-                navigate('/custcom');
-            }
+  const [username, setUsername] = useState('');
+  const [mobilenumber, setMobileno] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  var enableHover = 'false';
 
-            else if (response.data==="toUser") {
-              enableHover="false";
-              const queryParameters = `?mobno=${mobilenumber}&enable=${enableHover}`;
-          
-              navigate(`/home${queryParameters}`);
-            }
+  useEffect(() => {
+    document.body.classList.add(styles['login']);
+    return () => {
+      document.body.classList.remove(styles['login']);
+    };
+  }, []);
 
-            else if (response.data==="incorrectPassword") {
-                alert("Incorrect Password");
-            }
-            else{
-              console.log("error:");
-              console.log(response);
-             }
 
-          } catch (error) {
-            console.log("come to error");
-            console.error(error);
-          }
-        };
-  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await Axios.post('http://localhost:8080/login', {
+        username,
+        password,
+        mobilenumber
+      });
+
+
+      if (response.data === 'invalidUsername') {
+        alert("Invalid Username");
+      }
+
+      else if (response.data === "toAdmin") {
+        navigate('/custcom');
+      }
+
+      else if (response.data === "toUser") {
+        enableHover = "false";
+        const queryParameters = `?mobno=${mobilenumber}&enable=${enableHover}`;
+
+        navigate(`/home${queryParameters}`);
+      }
+
+      else if (response.data === "incorrectPassword") {
+        alert("Incorrect Password");
+      }
+      else {
+        console.log("error:");
+        console.log(response);
+      }
+
+    } catch (error) {
+      console.log("come to error");
+      console.error(error);
+    }
+  };
+
 
   return (
-    <div className='login_body'>
-    <div className='login_container'>
-      
-        <form onSubmit={handleSubmit}>
-            <div className="header">
-                <img src={logo} title="My Website" width="100px" height="90px" style={{display: "inline",}}/>
-        <h1 >Infinix</h1>
-            </div>
-            <h2 >Login Page</h2>
-            <div className="form-group">
-                <label>Username:</label>
-                <input type="text" name="username" placeholder="username" required="" id="id_username" onChange={e => setUsername(e.target.value)}/>
-            </div>
-            
-            <div className="form-group">
-                <label>Mobile Number:</label>
-                <input type="tel" id="mobile" placeholder="Mobile number" onChange={e => setMobileno(e.target.value)}/>
-            </div>
+   
+      <div className='login_container'>
 
-            <div className="form-group">
-                <label>Password:</label>
-                <div>
-                    <input type="password" name="password" placeholder="password"  required id="id_password" onChange={e => setPassword(e.target.value)}/>
-                </div>
+        <form onSubmit={handleSubmit}>
+          <div className="header">
+            <img src={logo} title="My Website" width="100px" height="90px" style={{ display: "inline", }} />
+            <h1 >Infinix</h1>
+          </div>
+          <h2 >Login Page</h2>
+          <div className="form-group">
+            <label>Username:</label>
+            <input type="text" name="username" placeholder="username" required="" id="id_username" onChange={e => setUsername(e.target.value)} />
+          </div>
+
+          <div className="form-group">
+            <label>Mobile Number:</label>
+            <input type="tel" id="mobile" placeholder="Mobile number" onChange={e => setMobileno(e.target.value)} />
+          </div>
+
+          <div className="form-group">
+            <label>Password:</label>
+            <div>
+              <input type="password" name="password" placeholder="password" required id="id_password" onChange={e => setPassword(e.target.value)} />
             </div>
-            
-                
-            <button type="submit" className="btn btn-primary">Login</button>
+          </div>
+
+
+          <button type="submit" className="btn btn-primary">Login</button>
         </form>
-    </div>
-     </div>
+      </div>
   );
 };
 
